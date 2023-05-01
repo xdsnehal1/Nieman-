c448301435f3163a746986547f6a1ed7ac1e7486
xdsnehal1 <132124326+xdsnehal1@users.noreply.github.com>
2023-05-01 09:56:40 +0530

    Update data.py

--- Romeo/helper/data.py
+++ Romeo/helper/data.py
@@ -3,7 +3,7 @@ from pyrogram.types import InlineKeyboardButton, WebAppInfo
 class Data:
 
     text_help_menu = (
-        "**ROMEOBOT PLUGINS**\n**COMMAND PREFIX;-** `.`"
+        "**Nieman PLUGINS**\n**COMMAND PREFIX;-** `.`"
         .replace(",", "")
         .replace("[", "")
         .replace("]", "")
