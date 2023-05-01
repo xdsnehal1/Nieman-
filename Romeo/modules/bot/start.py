e79dc12fbfe1ecaf45be88fb76cbfb5a1d656187
xdsnehal1 <132124326+xdsnehal1@users.noreply.github.com>
2023-05-01 09:47:34 +0530

    Update start.py

--- Romeo/modules/bot/start.py
+++ Romeo/modules/bot/start.py
@@ -4,4 +4,4 @@ from pyrogram import filters
 
 @app.on_message(filters.command("start") & filters.private)
 async def start(client, message):
-   await message.reply_text("Hey RomeoBot Assistant here")
+   await message.reply_text("Hey NiemanBot Assistant here")
